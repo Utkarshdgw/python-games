b233073cf4d27862ff2a4530e695c24f409d7686
Utkarshdgw <utkarsh.dwivedi2525@gmail.com>
2026-06-29 19:59:51 +0530

    Clean up input prompts in guess_the_number.py
    Removed inappropriate language from user prompts.

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,12 +3,12 @@ n = random.randint(1, 100)
 a = 0
 guesses = 1
 while(a != n):
-    a = int(input("Guess the number bitch: ")) 
+    a = int(input("Guess the number: ")) 
     if(a >n):
-        print("Lower number u dumb a** shit")
+        print("Lower number")
         guesses +=1
     elif(a<n):
-        print("choose a Higher number, looser mentality")
+        print("Higher number")
         guesses +=1
 
 print(f"You have guessed the number {n} correctly in {guesses} attempts")
